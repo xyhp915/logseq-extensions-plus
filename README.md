b223d08f518246d71f62459dd35ac13eed41de7f
charlie <xyhp915@qq.com>
2022-04-28 17:16:55 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # logseq-extensions-plus
+
 A plugin to enhance functions of Logseq's internal extensions.
+
+### Supported extensions
+
+- [x] [katex-physics](https://github.com/balthild/katex-physics)
+
+### Licence
+
+MIT
---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import '@logseq/libs'
+import { LSPluginBaseInfo, SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin'
+
+const Supports = {
+  katexPhysics: 'katexPhysics',
+}
+
+const settingsSchema: Array<SettingSchemaDesc> = [
+  {
+    key: 'katexPhysics',
+    type: 'boolean',
+    title: 'Support katex physics extension?',
+    description: 'KaTeX-physics from https://github.com/balthild/katex-physics.',
+    default: true,
+  },
+]
+
+function main (baseInfo: LSPluginBaseInfo) {
+  const { settings } = baseInfo
+
+  if (settings.katexPhysics) {
+    logseq.Experiments.registerExtensionsEnhancer('katex', async () => {
+      await logseq.Experiments.loadScripts(
+        './vendors/katex.physics.js',
+      )
+    })
+  }
+}
+
+// entry
+logseq.useSettingsSchema(settingsSchema).ready(main).catch(console.error)
